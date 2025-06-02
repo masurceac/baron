1713eff025b9a8669d62496033f032f772cf6595
Serghei Masurceac <masurceac@gmail.com>
2025-06-02 23:30:57 +0300

    chore: revert binance proxy

--- packages/bars-api/src/fetch-binance-bars.ts
+++ packages/bars-api/src/fetch-binance-bars.ts
@@ -36,7 +36,7 @@ export async function fetchBinanceBars(input: {
     throw new Error('Invalid start or end date');
   }
 
-  const baseUrl = 'https://api.binance.com/api/v3/klines'; // proxy server
+  const baseUrl = 'https://api.tursotopostgres.com/binance/api/v3/klines'; // proxy server
 
   const bars: ChartBar[] = [];
   const limit = 1000; // Binance max limit per request
